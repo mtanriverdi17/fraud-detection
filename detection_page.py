d325207fea252158c2a2675709fb84c4bf97ac24
mtanriverdi17 <48198540+mtanriverdi17@users.noreply.github.com>
2021-06-06 20:09:19 +0300

    Add files via upload

--- detection_page.py
+++ detection_page.py
@@ -26,13 +26,14 @@ raw_df = pd.read_csv(
     'https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv')
 
 
-st.write(raw_df.head())
+st.write('Below is the head of our dataset.', raw_df.head())
 
 
 neg, pos = np.bincount(raw_df['Class'])
 total = neg + pos
-print('Examples:\n    Total: {}\n    Positive: {} ({:.2f}% of total)\n'.format(
-    total, pos, 100 * pos / total))
+""" print('Examples:\n    Total: {}\n    Positive: {} ({:.2f}% of total)\n'.format(total, pos, 100 * pos / total)) """
+st.write('Examples\n Total: ', total, '\nPositive: ',
+         pos, ' ', 100*pos/total, '% OF TOTAL')
 
 cleaned_df = raw_df.copy()
 
@@ -81,9 +82,6 @@ test_labels = np.array(test_df.pop('Class'))
 train_features = np.array(train_df)
 val_features = np.array(val_df)
 test_features = np.array(test_df)
-
-
-print(train_df.shape)
 
 scaler = StandardScaler()
 train_features = scaler.fit_transform(train_features)
@@ -96,13 +94,20 @@ val_features = np.clip(val_features, -5, 5)
 test_features = np.clip(test_features, -5, 5)
 
 
-print('Training labels shape:', train_labels.shape)
-print('Validation labels shape:', val_labels.shape)
-print('Test labels shape:', test_labels.shape)
+st.write('Training labels shape:', train_labels.shape)
+st.write('Validation labels shape:', val_labels.shape)
+st.write('Test labels shape:', test_labels.shape)
 
-print('Training features shape:', train_features.shape)
-print('Validation features shape:', val_features.shape)
-print('Test features shape:', test_features.shape)
+st.write('Training features shape:', train_features.shape)
+st.write('Validation features shape:', val_features.shape)
+st.write('Test features shape:', test_features.shape)
+
+st.markdown("### Look at the data distribution")
+st.markdown('Next compare the distributions of the positive and negative examples over a few features. Good questions to ask yourself at this point are:')
+st.markdown("* Do these distributions make sense? ",
+            "* Yes. You've normalized the input and these are mostly concentrated in the `+/- 2` range.")
+st.markdown("* Can you see the difference between the distributions?",
+            "* Yes the positive examples contain a much higher rate of extreme values.")
 
 pos_df = pd.DataFrame(
     train_features[bool_train_labels], columns=train_df.columns)
